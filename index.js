29aaff35e82666bde6db8d7c178254035f6610b4
tichnas <sanchit.arora.2002@gmail.com>
2022-09-28 20:13:03 +0530

    Deployment ready

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const connectDB = require('./utils/connectDB');
 
@@ -7,11 +8,16 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/api', (req, res) => res.send('API Running'));
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/balance', require('./routes/api/balance'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
